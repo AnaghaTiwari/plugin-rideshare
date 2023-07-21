eb583af7108f7ea5d346acabb1947dadb10dced2
Anagha Tiwari <76963992+AnaghaTiwari@users.noreply.github.com>
2023-07-21 00:07:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -245,9 +245,142 @@ class Yolov8:
             
 
         # Return the modified input image
+        
         return input_image
 
-    def main(self, sample):
+
+    # postprocess_s2 - specifically for not publishing crop sticker
+    def postprocess(self, sample, plugin, args, output):
+        """
+        Performs post-processing on the model's output to extract bounding boxes, scores, and class IDs.
+
+        Args:
+            input_image (numpy.ndarray): The input image.
+            output (numpy.ndarray): The results/output of model
+            plugin: plugin to save output image to
+
+        Returns:
+            numpy.ndarray: The input image with detections drawn on it.
+        """
+
+        #####################
+        # timestamp = sample.timestamp
+        timestamp = 946706400000000000
+        # Transpose and squeeze the output to match the expected shape
+
+        ######################################
+        # outputs = np.transpose(np.squeeze(output[0]))
+        # rows = outputs.shape[0]
+            
+        # Lists to store the bounding boxes, scores, and class IDs of the detections
+        boxes = []
+        scores = []
+        class_ids = []
+
+        # Calculate the scaling factors for the bounding box coordinates
+        x_factor = self.img_width / self.input_width
+        y_factor = self.img_height / self.input_height
+
+        # Iterate over each output in the outputs array
+        ################ NOT NEEDD #########
+        
+        # for i in ouput:
+        #     # Extract the class scores from the current row
+        #     classes_scores = outputs[i][4:]
+
+        #     # Find the maximum score among the class scores
+        #     max_score = np.amax(classes_scores)
+
+        #     # If the maximum score is above the confidence threshold
+        #     if max_score >= self.confidence_thres:
+        #         # Get the class ID with the highest score
+        #         class_id = np.argmax(classes_scores)
+
+        #         # Extract the bounding box coordinates from the current row
+        #         x, y, w, h = outputs[i][0], outputs[i][1], outputs[i][2], outputs[i][3]
+
+        #         # Calculate the scaled coordinates of the bounding box
+        #         left = int((x - w / 2) * x_factor)
+        #         top = int((y - h / 2) * y_factor)
+        #         width = int(w * x_factor)
+        #         height = int(h * y_factor)
+
+        #         # Add the class ID, score, and box coordinates to the respective lists
+        #         class_ids.append(class_id)
+        #         scores.append(max_score)
+        #         boxes.append([left, top, width, height])
+
+        # Apply non-maximum suppression to filter out overlapping bounding boxes
+        # indices = cv2.dnn.NMSBoxes(boxes, scores, self.confidence_thres, self.iou_thres)
+        
+        detection_stats = 'found: '
+        found = {}
+        # Iterate over the selected indices after non-maximum suppression
+
+
+
+        ###############################
+        # input_image = sample.data
+        input_image = cv2.imread(sample)        
+        
+
+        ############# NOT NEEDED ##############
+        # for i in indices:
+        #     # Get the box, score, and class ID corresponding to the index
+        #     box = boxes[i]
+        #     score = scores[i]
+        #     class_id = class_ids[i]
+            
+        #     # Draw the detection on the input frame, and save to plugin using cv2
+        #     self.draw_detections(input_image, box, score, class_id)
+        #     cv2.imwrite('yolov8.jpg', input_image)
+        #     plugin.upload_file('yolov8.jpg')
+        #     print('image saved')
+
+        #     if not class_id in found:
+        #         found[class_id] = 1
+        #     else:
+        #         found[class_id] += 1
+        count = 0
+        name = []
+        for result in output:
+            res_plotted = result.plot()
+            cv2.imwrite('result.jpeg', res_plotted)
+            plugin.upload_file('result.jpeg')
+
+            boxes = result.boxes.cpu().numpy()                         # get boxes on cpu in numpy
+            for box in boxes:                                          # iterate boxes
+                name.append(result.names[int(box.cls[0])])
+                count += 1
+
+        for result in output:
+            boxes = result.boxes.cpu().numpy()
+            for i, box in enumerate(boxes):
+                r = box.xyxy[0].astype(int)
+                crop = input_image[r[1]:r[3], r[0]:r[2]]
+                cv2.imwrite("crop.jpeg", crop)
+                plugin.upload_file("crop.jpeg")
+            
+        # print detection stats
+        TOPIC_TEMPLATE = 'rideshare'
+        # for name, count in found.items():
+        for i in name:
+            # detection_stats += f'{class_id}[{count}] '
+            detection_stats += f'{i}, {count}'
+            # plugin.publish(f'{class_id}', count, timestamp = timestamp)
+            # plugin.publish(f'{i}', count, timestamp)
+            # plugin.publish(f'{TOPIC_TEMPLATE}.{str(i)}', str(count), str(timestamp))
+            
+            plugin.publish('rideshare.log', str(count), timestamp=timestamp)
+            
+        print(detection_stats)
+            
+
+        # Return the modified input image
+        
+        return input_image
+
+    def main(self, sample, stage):
         """
         Performs inference using YoloV8 model and returns the output image frame with drawn detections.
 
@@ -259,7 +392,6 @@ class Yolov8:
         ####################
         # model_inputs = sample.data
         model_inputs = cv2.imread(sample)
-        print(type(model_inputs))
         # Store the shape of the input for later use
         ####################
         # input_shape = model_inputs[0].shape
@@ -283,7 +415,11 @@ class Yolov8:
             
         # Perform post-processing on the outputs to obtain output image.
         # output_img = self.postprocess(self.img, outputs)
-        output_img = self.postprocess(sample, plugin, args, outputs)
+        # Check for stage (if 2, then final so don't publish anything)
+        if (stage == 2):
+            output_img = self.postprocess_s2(sample, plugin, args, outputs) 
+        else:
+            output_img = self.postprocess(sample, plugin, args, outputs)
 
         # Return the resulting output image
         return output_img
@@ -293,8 +429,9 @@ if __name__ == '__main__':
     # Create an argument parser to handle command-line arguments
     parser = argparse.ArgumentParser(description = "Evaluate YoloV8 model")
     parser.add_argument('--model', type=str, default='yolov8.pt', help='Input YoloV8 model.')
+    parser.add_argument('--model2', type=str, default='best_s2.pt', help='Input stage2 YoloV8 model.')
     parser.add_argument('-stream', type=str, action='store', default=str('bottom'), help='ID of stream')
-    parser.add_argument('--conf-thres', type=float, default=0.5, help='Confidence threshold')
+    parser.add_argument('--conf-thres', type=float, default=0.2, help='Confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.5, help='IoU threshold')
     parser.add_argument('-continuous', dest = 'continuous', action='store_true', default=False, help='Continuous run flag T/F')
     args = parser.parse_args()
@@ -316,6 +453,8 @@ if __name__ == '__main__':
 
     # Create an instance of the Yolov8 class with the specified arguments
     detection = Yolov8(args.model, args.stream, args.conf_thres, args.iou_thres)
+    
+    detection2 = Yolov8(args.model2, args.stream, 0.4, args.iou_thres)
 
     # take sample from plugin camera
     while True:
@@ -326,9 +465,12 @@ if __name__ == '__main__':
                 # sample = camera.snapshot()
                 sample = 'test.jpeg'
 
-            # Perform object detection - return output
+            # Perform object detection - return crop - stage 1
+            crop = detection.main(sample, 1)
 
-            output_image = detection.main(sample)
+            # Perform object detection - return for stage 2
+            output = detection2.main(crop, 2)
+            
             if not args.continuous:
                 exit(0)
         
